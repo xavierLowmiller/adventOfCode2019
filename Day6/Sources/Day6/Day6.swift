f06ea897f4bf91da5c79d04466f17c2c38ad770c
Xavier Lowmiller <xaver.lohmueller@me.com>
2019-12-06 09:00:49 +0100

    Remove unnecessary ceremony

--- Day6/Sources/Day6/Day6.swift
+++ Day6/Sources/Day6/Day6.swift
@@ -1,60 +1,46 @@
 struct Orbit {
-	private let solarSystem: [String: SpaceObject]
+	private let solarSystem: [String: String]
 
 	init(input: String) {
-		solarSystem = parseInput(input: input)
-	}
+		solarSystem = input
+			.split(separator: "\n")
+			.reduce(into: [:]) { (result, orbitFact) in
+				let id = String(orbitFact.split(separator: ")")[1])
+				let orbitedObjectId = String(orbitFact.split(separator: ")")[0])
 
-	var checksum: Int {
-		solarSystem
-			.map { $0.value }
-			.reduce(into: 0) { (result, spaceObject) in
-				result += spaceObject.orbitChain(in: solarSystem).count
+				result[id] = orbitedObjectId
 		}
 	}
 
-	func jumpsRequired(between id1: String, and id2: String) -> Int {
-		let object1 = solarSystem[id1]!
-		let object2 = solarSystem[id2]!
-
-		let orbitChain1 = object1.orbitChain(in: solarSystem)
-		let orbitChain2 = object2.orbitChain(in: solarSystem)
-
-		let commonSubsequence = zip(orbitChain1.reversed(), orbitChain2.reversed())
-			.filter { $0.0 == $0.1 }
-			.map { $0.0 }
-			.reversed()
-
-		return orbitChain1.dropLast(commonSubsequence.count).count
-			+ orbitChain2.dropLast(commonSubsequence.count).count
-	}
-}
-
-private struct SpaceObject {
-	let id: String
-	let orbitedObjectId: String
-
-	func orbitChain(in solarSystem: [String: SpaceObject]) -> [String] {
+	private func orbitChain(for id: String) -> [String] {
 		var chain: [String] = [id]
 
-		var id = orbitedObjectId
+		var id = id
 		while id != "COM" {
-			id = solarSystem[id]!.orbitedObjectId
+			id = solarSystem[id]!
 			chain.append(id)
 		}
 
 		return chain
 	}
-}
 
+	var checksum: Int {
+		solarSystem
+			.map { $0.value }
+			.reduce(into: 0) { (result, id) in
+				result += orbitChain(for: id).count
+		}
+	}
 
-private func parseInput(input: String) -> [String: SpaceObject] {
-	input
-		.split(separator: "\n")
-		.reduce(into: [:]) { (result, orbitFact) in
-			let id = String(orbitFact.split(separator: ")")[1])
-			let orbitedObjectId = String(orbitFact.split(separator: ")")[0])
+	func jumpsRequired(between id1: String, and id2: String) -> Int {
+		let orbitChain1 = orbitChain(for: id1)
+		let orbitChain2 = orbitChain(for: id2)
 
-			result[id] = SpaceObject(id: id, orbitedObjectId: orbitedObjectId)
+		let commonSubsequenceLength = zip(orbitChain1.reversed(), orbitChain2.reversed())
+			.filter { $0.0 == $0.1 }
+			.count
+
+		return orbitChain1.count - commonSubsequenceLength - 1
+			+ orbitChain2.count - commonSubsequenceLength - 1
 	}
 }

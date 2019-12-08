46c30e46d941805b5886b37cb05852f9cd9eec22
Xavier Lowmiller <xaver.lohmueller@me.com>
2019-12-08 08:04:09 +0100

    Solve Day 8 part 2

--- Day8/Sources/Day8/Day8.swift
+++ Day8/Sources/Day8/Day8.swift
@@ -1,19 +1,33 @@
 struct Image {
-	let layers: [String]
+	let width: Int
+	let height: Int
+	let layers: [[Character]]
 
 	init(input: String, width: Int, height: Int) {
-		let pixelsPerLayer = width * height
+		self.width = width
+		self.height = height
 
+		let pixelsPerLayer = width * height
 		layers = input.chunked(into: pixelsPerLayer)
-			.map { String($0) }
 	}
 
 	var checksum: Int {
 		guard !layers.isEmpty, !layers[0].isEmpty else { return 0 }
-		let layerWithFewestZeros = layers.min { $0.countOfZeros < $1.countOfZeros }!
+
+		let layerWithFewestZeros = layers
+			.min { $0.filter { $0 == "0" }.count < $1.filter { $0 == "0" }.count }!
 		let amountOf1 = layerWithFewestZeros.filter { $0 == "1" }.count
 		let amountOf2 = layerWithFewestZeros.filter { $0 == "2" }.count
 		return amountOf1 * amountOf2
+	}
+
+	var decoded: [String] {
+		(0..<width * height)
+			.reduce(into: "") { result, index in
+				let layeredPixels = layers.map { $0[index] }
+				result.append(layeredPixels.first { $0 != "2" } ?? "0")
+			}
+			.chunked(into: width).map { String($0) }
 	}
 }
 
@@ -28,11 +42,5 @@ private extension Sequence {
 			}
 
 		}
-	}
-}
-
-private extension String {
-	var countOfZeros: Int {
-		filter { $0 == "0" }.count
 	}
 }
--- Day8/Tests/Day8Tests/Day8Tests.swift
+++ Day8/Tests/Day8Tests/Day8Tests.swift
@@ -5,16 +5,42 @@ final class Day8Tests: XCTestCase {
 	func testExampleDay8Part1() {
 		// Given
 		let input = "123456789012"
-
-		// When
 		let image = Image(input: input, width: 3, height: 2)
 
+		// When
+		let checksum = image.checksum
+
 		// Then
-		XCTAssertEqual(image.checksum, 1)
+		XCTAssertEqual(checksum, 1)
+	}
+
+	func testExampleDay8Part2() {
+		// Given
+		let input = "0222112222120000"
+		let image = Image(input: input, width: 2, height: 2)
+
+		// When
+		let decoded = image.decoded
+
+		// Then
+		XCTAssertEqual(decoded, ["01", "10"])
 	}
 
 	func testDay8Part1() {
 		let image = Image(input: input, width: 25, height: 6)
 		print("Solution to day 8 part 1:", image.checksum)
+	}
+
+	func testDay8Part2() {
+		let image = Image(input: input, width: 25, height: 6)
+		print("Solution to day 8 part 2:")
+		print(image.decoded.pretty)
+	}
+}
+
+private extension Array where Element == String {
+	var pretty: String {
+		reduce("") { $0 + $1 + "\n" }
+			.replacingOccurrences(of: "0", with: " ")
 	}
 }

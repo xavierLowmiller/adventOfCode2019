d152a62a2608b47b102c4bfa9ee7888d9bdd5775
Xavier Lowmiller <xaver.lohmueller@me.com>
2019-12-14 15:33:46 +0100

    Refactor to simpler types

--- Day14/Sources/Day14/Day14.swift
+++ Day14/Sources/Day14/Day14.swift
@@ -1,113 +1,75 @@
 import Foundation
 
-struct Material: Hashable {
-	var amount: UInt
-	let name: String
-
-	var isOre: Bool {
-		name == "ORE"
-	}
-}
-
-extension Material: CustomStringConvertible {
-	init(_ string: String) {
-		amount = UInt(string.split(separator: " ")[0])!
-		name = String(string.split(separator: " ")[1])
-	}
-
-	var description: String {
-		"\(amount) \(name)"
-	}
-}
-
-struct System {
-	let cookbook: [Material: [Material]]
+final class System {
+	let cookbook: [String: (ingredients: [String: UInt], produces: UInt)]
 
 	init(string: String) {
 		let lines = string.components(separatedBy: "\n")
+
+		func separate(string: String) -> (name: String, amount: UInt) {
+			(string.components(separatedBy: " ")[1],
+			 UInt(string.components(separatedBy: " ")[0])!)
+		}
+
 		cookbook = lines.reduce(into: [:]) { result, line in
 			let pair = line
 				.components(separatedBy: "=>")
 				.map { $0.trimmingCharacters(in: .whitespaces)}
-			let key = Material(pair[1])
-			let values = pair[0].components(separatedBy: ", ").map(Material.init)
-			result[key] = values
+			let (key, amount) = separate(string: pair[1])
+			let required = Dictionary(uniqueKeysWithValues: pair[0].components(separatedBy: ", ").map(separate))
+			result[key] = (required, amount)
 		}
 	}
 
-	func calculateOreNeeded() -> UInt {
-		var neededForFuel = cookbook[.init("1 FUEL")]!
-		var surplus: [Material] = []
+	func calculateOreNeeded(for fuel: UInt = 1) -> UInt {
+		var cookbook = self.cookbook
+		var ingredientsForFuel = cookbook.removeValue(forKey: "FUEL")!.ingredients.mapValues { $0 * fuel }
 
-		while var materialToReplace = neededForFuel.first(where: { !$0.isOre }) {
-			neededForFuel = neededForFuel.filter { $0 != materialToReplace }
+		var surplus: [String: UInt] = [:]
 
-			// Use Surplus
-			if var existingMaterial = surplus.first(where: { $0.name == materialToReplace.name }) {
-				let diff = Int(materialToReplace.amount) - Int(existingMaterial.amount)
-				if diff > 0 {
-					materialToReplace.amount -= existingMaterial.amount
-					surplus = surplus.filter { $0 != existingMaterial }
-				} else if diff == 0 {
-					neededForFuel = neededForFuel.filter { $0 != materialToReplace }
-					surplus = surplus.filter { $0 != existingMaterial }
-					continue
-				} else if diff < 0 {
-					neededForFuel = neededForFuel.filter { $0 != materialToReplace }
-					let indexOfSurplus = surplus.firstIndex(where: { $0.name == materialToReplace.name })!
-					existingMaterial.amount -= materialToReplace.amount
-					surplus[indexOfSurplus] = existingMaterial
-					continue
+		while let materialToReplace = ingredientsForFuel.keys.first(where: { $0 != "ORE" }),
+			var amountToReplace = ingredientsForFuel.removeValue(forKey: materialToReplace) {
+
+				if let amountInStock = surplus[materialToReplace] {
+					let diff = Int(amountToReplace) - Int(amountInStock)
+
+					switch diff {
+					case ..<0:
+						surplus[materialToReplace] = amountInStock - amountToReplace
+						continue
+					case 0:
+						surplus[materialToReplace] = nil
+						continue
+					default:
+						amountToReplace -= amountInStock
+						surplus[materialToReplace] = nil
+					}
 				}
-			}
 
-			let replacement = findReplacement(for: materialToReplace)
-			neededForFuel += replacement.needed
-			surplus.append(replacement.surplus)
+				let replacement = findReplacement(for: materialToReplace, amountNeeded: amountToReplace)
 
-			neededForFuel = neededForFuel.reduced
-			surplus = surplus.reduced
+				for ingredient in replacement.needed {
+					ingredientsForFuel[ingredient.key, default: 0] += ingredient.value
+				}
+
+				surplus[materialToReplace, default: 0] += replacement.surplus
 		}
 
-		assert(neededForFuel.count == 1)
-		assert(neededForFuel.first!.isOre)
+		assert(ingredientsForFuel.count == 1)
 
-		return neededForFuel.first!.amount
+		return ingredientsForFuel["ORE"]!
 	}
 
-	func findReplacement(for material: Material) -> (needed: [Material], surplus: Material) {
-		let reaction = cookbook.first { $0.key.name == material.name }!
+	private func findReplacement(for material: String, amountNeeded: UInt) -> (needed: [String: UInt], surplus: UInt) {
+		let reaction = cookbook[material]!
 
-		let amountNeeded = material.amount
-		let amountProvided = reaction.key.amount
+		let amountProvided = reaction.produces
 		let factor = amountNeeded % amountProvided == 0
 			? amountNeeded / amountProvided
 			: amountNeeded / amountProvided + 1
 
-		let needed: [Material] = reaction.value.map {
-			var material = $0
-			material.amount *= factor
-			return material
-		}
-
 		let surplus = amountProvided * factor - amountNeeded
 
-		return (needed, Material(amount: surplus, name: material.name))
-	}
-}
-
-private extension Array where Element == Material {
-	/// Adds up elements of the same kind
-	/// i.e.: ["1 ORE", "2 A", "3 A"] -> ["1 ORE", "5 A"]
-	var reduced: [Element] {
-		let names = Set(map { $0.name })
-		return names.reduce(into: []) { result, name in
-			let totalAmount: UInt = filter { $0.name == name }
-				.map { $0.amount }
-				.reduce(0, +)
-
-			result.append(Material(amount: totalAmount, name: name))
-		}
-		.filter { $0.amount != 0 }
+		return (reaction.ingredients.mapValues { $0 * factor }, surplus)
 	}
 }

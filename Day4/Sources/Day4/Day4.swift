3544bcad36b82807a6d97ac7bb7c20b8bfc24227
Xavier Lowmiller <xaver.lohmueller@me.com>
2019-12-14 16:32:25 +0100

    Refactor Day 4 solution

--- Day4/Sources/Day4/Day4.swift
+++ Day4/Sources/Day4/Day4.swift
@@ -1,53 +1,33 @@
 func possiblePasswordsPart1() -> Int {
-	var count = 0
-
-	for p in 231832...767346 {
-		let s = String(p)
-		if s.isOnlyAscending, s.containsDuplicate {
-			count += 1
-		}
-	}
-
-	return count
+	(231832...767346)
+		.map(String.init)
+		.filter { $0.isOnlyAscending }
+		.filter { $0.containsDuplicate }
+		.count
 }
 
 func possiblePasswordsPart2() -> Int {
-	var count = 0
-
-	for p in 231832...767346 {
-		let s = String(p)
-		if s.isOnlyAscending, s.containsDuplicateThatIsRepeatedExactlyTwice {
-			count += 1
-		}
-	}
-
-	return count
+	(231832...767346)
+		.map(String.init)
+		.filter { $0.isOnlyAscending }
+		.filter { $0.containsExactDuplicate }
+		.count
 }
 
-private extension String {
+extension String {
 	var containsDuplicate: Bool {
-		let numberOfCharacters = reduce(into: [:]) { result, character in
-			result[character, default: 0] += 1
-		}
-
-		return numberOfCharacters.values.contains { $0 >= 2 }
+		Dictionary(grouping: self, by: { $0 })
+			.mapValues { $0.count }
+			.values.contains { $0 >= 2 }
 	}
 
-	var containsDuplicateThatIsRepeatedExactlyTwice: Bool {
-		let numberOfCharacters = reduce(into: [:]) { result, character in
-			result[character, default: 0] += 1
-		}
-
-		return numberOfCharacters.values.contains { $0 == 2 }
+	var containsExactDuplicate: Bool {
+		Dictionary(grouping: self, by: { $0 })
+			.mapValues { $0.count }
+			.values.contains { $0 == 2 }
 	}
 
 	var isOnlyAscending: Bool {
-		for (c1, c2) in zip(self, self.dropFirst()) {
-			if c1 > c2 {
-				return false
-			}
-		}
-
-		return true
+		zip(self, self.dropFirst()).allSatisfy(<=)
 	}
 }
--- Day4/Tests/Day4Tests/Day4Tests.swift
+++ Day4/Tests/Day4Tests/Day4Tests.swift
@@ -2,6 +2,110 @@ import XCTest
 @testable import Day4
 
 final class Day4Tests: XCTestCase {
+	func testContainsNumberMoreThanTwicePositive() {
+		// Given
+		let examples = [
+			111234,
+			123444,
+			123345
+		].map(String.init)
+
+		for example in examples {
+			// When
+			let containsDuplicate = example.containsDuplicate
+
+			// Then
+			XCTAssert(containsDuplicate)
+		}
+	}
+
+	func testContainsNumberMoreThanTwiceNegative() {
+		// Given
+		let examples = [
+			123456,
+			654321,
+			152369
+		].map(String.init)
+
+		for example in examples {
+			// When
+			let containsDuplicate = example.containsDuplicate
+
+			// Then
+			XCTAssertFalse(containsDuplicate)
+		}
+	}
+
+	func testContainsNumberExactlyTwicePositive() {
+		// Given
+		let examples = [
+			112345,
+			114444,
+			445678
+		].map(String.init)
+
+		for example in examples {
+			// When
+			let containsExactDuplicate = example.containsExactDuplicate
+
+			// Then
+			XCTAssert(containsExactDuplicate)
+		}
+	}
+
+	func testContainsNumberExactlyTwiceNegative() {
+		// Given
+		let examples = [
+			111444,
+			111234,
+			123456
+		].map(String.init)
+
+		for example in examples {
+			// When
+			let containsExactDuplicate = example.containsExactDuplicate
+
+			// Then
+			XCTAssertFalse(containsExactDuplicate)
+		}
+	}
+
+	func testIsAscendingPositive() {
+		// Given
+		let examples = [
+			111444,
+			111234,
+			123456,
+			111111
+		].map(String.init)
+
+		for example in examples {
+			// When
+			let isOnlyAscending = example.isOnlyAscending
+
+			// Then
+			XCTAssert(isOnlyAscending)
+		}
+	}
+
+	func testIsAscendingNegative() {
+		// Given
+		let examples = [
+			411144,
+			912345,
+			654321,
+			111110
+		].map(String.init)
+
+		for example in examples {
+			// When
+			let isOnlyAscending = example.isOnlyAscending
+
+			// Then
+			XCTAssertFalse(isOnlyAscending)
+		}
+	}
+
 	func testDay4SolutionPart1() {
 		print("Solution to day 4 part 1:", possiblePasswordsPart1())
 	}
